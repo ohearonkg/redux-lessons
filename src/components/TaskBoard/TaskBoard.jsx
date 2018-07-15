10493ea9b16c52be88d4b661f94ecb61a9165c0e
K.G. O'Hearon <ohearonkg@gmail.com>
2018-07-15 20:39:05 -0300

    now have the very basic loading indicator.

--- src/components/TaskBoard/TaskBoard.jsx
+++ src/components/TaskBoard/TaskBoard.jsx
@@ -37,39 +37,46 @@ const TaskColumnTitle = styled.h2`
 `;
 
 const TaskBoard = props => {
-  return (
-    <TaskBoardWrapper>
+  if (props.loading) {
+    return (
+      <h1> Loading... </h1>
+    )
+  } else {
+    return (
+      <TaskBoardWrapper>
 
-      {!props.addToDoFormShown && 
-        <React.Fragment>
-          <AddToDoButtonWrapper>
-            <CustomButton text="Add To Do" onClickFunction={props.toggleAddToDoForm} />
-          </AddToDoButtonWrapper>
+        {!props.addToDoFormShown && 
+          <React.Fragment>
+            <AddToDoButtonWrapper>
+              <CustomButton text="Add To Do" onClickFunction={props.toggleAddToDoForm} />
+            </AddToDoButtonWrapper>
 
-          <TaskBoardColumnsWrapper>
-            {sampleStatuses.map( (status,index) => {
-              return (
-                <TaskColumnWrapper key={index}>
-                  <TaskColumnTitle> {status} </TaskColumnTitle>
-                  <TaskList tasks={props.tasks.filter( task => task.status === status)} />
-                </TaskColumnWrapper>
-              )
-            })}
-          </TaskBoardColumnsWrapper>
-        </React.Fragment>
-      }
+            <TaskBoardColumnsWrapper>
+              {sampleStatuses.map( (status,index) => {
+                return (
+                  <TaskColumnWrapper key={index}>
+                    <TaskColumnTitle> {status} </TaskColumnTitle>
+                    <TaskList tasks={props.tasks.filter( task => task.status === status)} />
+                  </TaskColumnWrapper>
+                )
+              })}
+            </TaskBoardColumnsWrapper>
+          </React.Fragment>
+        }
 
-      {props.addToDoFormShown && 
-        <NewTaskForm />
-      }
-      
-    </TaskBoardWrapper>
-  )
+        {props.addToDoFormShown && 
+          <NewTaskForm />
+        }
+        
+      </TaskBoardWrapper>
+    )
+  }
 }
 
 const mapStateToProps = state => {
   return {
-    addToDoFormShown: state.addToDoFormShown,
+    addToDoFormShown: state.tasksReducer.addToDoFormShown,
+    loading: state.tasksReducer.loading
   }
 }
 
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -13,21 +13,15 @@ const TaskWrapper = styled.div`
 `;
 
 const TaskList = props => {
-  if (props.tasks.length === 0) {
-    return (
-      <h1> Loading... </h1>
-    )
-  } else {
-    return (
-      <TaskListWrapper>
-        {props.tasks.map( (task,index) => 
-          <TaskWrapper key={index}>
-            <Task id={task.id} title={task.title} description={task.description} status={task.status} />
-          </TaskWrapper>
-        )}
-      </TaskListWrapper>
-    ) 
-  }
+  return (
+    <TaskListWrapper>
+      {props.tasks.map( (task,index) => 
+        <TaskWrapper key={index}>
+          <Task id={task.id} title={task.title} description={task.description} status={task.status} />
+        </TaskWrapper>
+      )}
+    </TaskListWrapper>
+  ) 
 }
 
 export default TaskList;
